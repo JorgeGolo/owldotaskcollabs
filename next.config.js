c546aacbde24c80ddee74b6b233a2355e4438fe1
Jorge Gómez López <jorgelogomez@gmail.com>
2025-06-17 12:17:57 +0200

    resolving conflicts

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-const BUILD_DATE = new Date().toISOString().split("T")[1]; // formato YYYY-MM-DD
-const packageJson = require("./package.json");
+const BUILD_DATE = new Date().toISOString().split('T')[1]; // formato YYYY-MM-DD
+const packageJson = require('./package.json');
 const APP_VERSION = packageJson.version;
 
 // Configuración de Next.js
 const nextConfig = {
   trailingSlash: true, // Añade una barra diagonal al final de las URLs (ej., /about/ en lugar de /about)
-  output: "export", // Usa el modo "export" de Next.js para generar un sitio estático
+  output: 'export', // Usa el modo "export" de Next.js para generar un sitio estático
   images: {
     unoptimized: true, // Indica que las imágenes no deben ser optimizadas durante la build (útil con 'output: export')
   },
@@ -15,42 +15,42 @@ const nextConfig = {
 };
 
 // Configuración de Next-PWA (Progressive Web App)
-const withPWA = require("next-pwa")({
-  dest: "public", // Directorio donde se generarán los archivos del Service Worker y manifest.json
+const withPWA = require('next-pwa')({
+  dest: 'public', // Directorio donde se generarán los archivos del Service Worker y manifest.json
   register: true, // Indica si se debe registrar el Service Worker automáticamente
   skipWaiting: true, // Fuerza al nuevo Service Worker a activarse inmediatamente, sin esperar a que se cierren las pestañas anteriores
 
   // Precaching automático de rutas SSG (Next.js export)
   additionalManifestEntries: (() => {
-    const fs = require("fs");
-    const path = require("path");
-    let routes = ["/", "/offline/"]; // Rutas predeterminadas
+    const fs = require('fs');
+    const path = require('path');
+    let routes = ['/', '/offline/']; // Rutas predeterminadas
 
     try {
       // Leer el archivo routesprecatch.json de la carpeta public
       const routesFilePath = path.join(
         __dirname,
-        "public",
-        "routesprecache.json",
+        'public',
+        'routesprecache.json',
       );
 
       if (fs.existsSync(routesFilePath)) {
-        const routesData = fs.readFileSync(routesFilePath, "utf8");
+        const routesData = fs.readFileSync(routesFilePath, 'utf8');
         const parsedRoutes = JSON.parse(routesData);
 
         // Verifica que el contenido sea un array
         if (Array.isArray(parsedRoutes) && parsedRoutes.length > 0) {
           routes = parsedRoutes;
-          console.log("Rutas cargadas correctamente para precaching:", routes);
+          console.log('Rutas cargadas correctamente para precaching:', routes);
         }
       } else {
         console.log(
-          "Archivo routesprecatch.json no encontrado, usando rutas predeterminadas",
+          'Archivo routesprecatch.json no encontrado, usando rutas predeterminadas',
         );
       }
     } catch (error) {
       console.error(
-        "Error al leer el archivo de rutas para precaching:",
+        'Error al leer el archivo de rutas para precaching:',
         error,
       );
     }
@@ -63,9 +63,9 @@ const withPWA = require("next-pwa")({
     // Configuración del cacheo en tiempo de ejecución (para recursos dinámicos)
     {
       urlPattern: /\/quizzes\/.*/, // Patrón de URL para las rutas relacionadas con "quizzes"
-      handler: "NetworkFirst", // Estrategia de caché: intenta obtener de la red primero, luego de la caché si falla
+      handler: 'NetworkFirst', // Estrategia de caché: intenta obtener de la red primero, luego de la caché si falla
       options: {
-        cacheName: "quizzes-cache", // Nombre de la caché para estos recursos
+        cacheName: 'quizzes-cache', // Nombre de la caché para estos recursos
         networkTimeoutSeconds: 3, // Tiempo máximo de espera para la red antes de usar la caché (en segundos)
         expiration: {
           maxEntries: 100, // Máximo número de entradas en la caché
@@ -87,18 +87,18 @@ const withPWA = require("next-pwa")({
     },
     {
       urlPattern: /\.(png|svg|ico|jpg)$/,
-      handler: "StaleWhileRevalidate",
+      handler: 'StaleWhileRevalidate',
       options: {
-        cacheName: "static-assets", // Usará el hash real
+        cacheName: 'static-assets', // Usará el hash real
         expiration: { maxEntries: 100, maxAgeSeconds: 86400 },
       },
     },
     // **AÑADIR ESTA SECCIÓN PARA LAS PORTADAS DE LIBROS**
     {
       urlPattern: /\/assets\/bookcovers\/.*/, // **AJUSTADO: sin el dominio completo**
-      handler: "CacheFirst", // O 'NetworkFirst' según tu preferencia
+      handler: 'CacheFirst', // O 'NetworkFirst' según tu preferencia
       options: {
-        cacheName: "bookcovers-cache",
+        cacheName: 'bookcovers-cache',
         expiration: {
           maxEntries: 500, // Ajusta el número de entradas
           maxAgeSeconds: 60 * 24 * 60 * 60, // Ejemplo: 60 días
