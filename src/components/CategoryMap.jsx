1888d1557d3816a7df0786424ac0004c8a72bc1d
Jorge Gómez López <jorgelogomez@gmail.com>
2025-06-28 08:12:16 +0200

    new version

--- src/components/CategoryMap.jsx
+++ src/components/CategoryMap.jsx
@@ -95,7 +95,7 @@ const CategoryMap = ({ quizzes, limit = null }) => {
   return (
     <motion.div
       ref={ref}
-      className={`grid grid-cols-3 md:grid-cols-6 lg:grid-cols-6 gap-2 py-4 ${
+      className={`grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-2 py-4 ${
         categoryCount < 8 ? 'justify-center' : ''
       }`}
       variants={containerVariants}
